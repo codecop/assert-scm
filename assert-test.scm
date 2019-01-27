272d39af0a83055a33a97998f601c5a9a92b0056
Peter Kofler <peter.kofler@code-cop.org>
2019-01-27 01:01:44 +0100

    Latest version of SUnit with ANSI colours, useless test, a few more assertions and two bug fixes in list=?.

--- assert-test.scm
+++ assert-test.scm
@@ -2,20 +2,22 @@
 
 (define (test-failure name expected-message body)
     (test-case name
-        (assert-raise
-            (string-append "AssertionError: " expected-message)
-            body)))
+        (assert-raise (-make-assertion-error expected-message)
+                      body)))
 
 (test-failure "(fail) throws AssertionError with message"
     "message"
-    (lambda () (fail "message")))
+    (lambda ()
+        (fail "message")))
 
 (test-case "(check) should not fail on #t condition"
-    (lambda () (check "should not fail" #t)))
+    (lambda ()
+        (check "should not fail" #t)))
 
 (test-failure "(check) fails on #f condition"
     "message"
-    (lambda () (check "message" #f)))
+    (lambda ()
+        (check "message" #f)))
 
 (test-case "(assert=) equals number"
     (assert= 1 1))
@@ -39,34 +41,59 @@
     (assert-inexact= 1. 1.1 0.01))
 
 (test-case "(assert-list=)"
-    (assert-list= number->string = (list 1 2) (list 1 2)))
+    (assert-list= number->string
+                  =
+                  (list 1 2)
+                  (list 1 2)))
 
 (test-failure "(assert-list=) fails on wrong element"
     "2. item expected:<3> but was:<2>"
-    (assert-list= number->string = (list 1 3) (list 1 2)))
+    (assert-list= number->string
+                  =
+                  (list 1 3)
+                  (list 1 2)))
 
 (test-failure "(assert-list=) fails on short list"
     "3. item expected:<more elements> but was:<no more elements>"
-    (assert-list= number->string = (list 1 2 3) (list 1 2)))
+    (assert-list= number->string
+                  =
+                  (list 1 2 3)
+                  (list 1 2)))
 
 (test-failure "(assert-list=) fails on long list"
     "2. item expected:<no more elements> but was:<more elements>"
-    (assert-list= number->string = (list 1) (list 1 2)))
+    (assert-list= number->string
+                  =
+                  (list 1)
+                  (list 1 2)))
 
 (test-case "(assert-list=) recursive"
-    (assert-list= number->string = (list 1 (list 2 3)) (list 1 (list 2 3))))
+    (assert-list= number->string
+                  =
+                  (list 1 (list 2 3))
+                  (list 1 (list 2 3))))
 
 (test-failure "(assert-list=) recursive fails on wrong element type"
     "2. item expected:<a sublist> but was:<no sublist>"
-    (assert-list= number->string = (list 1 (list 3)) (list 1 2)))
+    (assert-list= number->string
+                  =
+                  (list 1 (list 3))
+                  (list 1 2)))
 
 (test-failure "(assert-list=) recursive fails on wrong element"
     "22. item expected:<3> but was:<4>"
-    (assert-list= number->string = (list 1 (list 2 3)) (list 1 (list 2 4))))
+    (assert-list= number->string
+                  =
+                  (list 1 (list 2 3))
+                  (list 1 (list 2 4))))
 
 (test-case "(assert-list=) with strings"
-    (assert-list= values string=? (list "a") (list "a"))
-    (assert-string-list= (list "a") (list "a")))
+    (assert-list= values
+                  string=?
+                  (list "a")
+                  (list "a"))
+    (assert-string-list= (list "a")
+                         (list "a")))
 
 (test-case "(assert-true)"
     (assert-true #t))
@@ -96,37 +123,95 @@
     "expected:<not null> but was:<null>"
     (assert-not-null '()))
 
-(test-case "(assert-raise) on raise"
-    (assert-raise
-        'a
-        (lambda () (raise 'a))))
+(test-case "(assert-raise) on raise symbol"
+    (assert-raise 'a (lambda ()
+                         (raise 'a))))
 
 (test-case "(assert-raise) on raise string"
-    (assert-raise
-        "a"
-        (lambda () (raise 'a))))
+    (assert-raise "a" (lambda ()
+                          (raise 'a))))
 
-(test-case "(assert-raise) on error"
-    (assert-raise
-        'a
-        (lambda () (error 'a))))
+(test-case "(assert-raise) on error symbol"
+    (assert-raise 'a (lambda ()
+                         (error 'a))))
 
 (test-case "(assert-raise) on error string"
-    (assert-raise
-        "a"
-        (lambda () (error "a"))))
+    (assert-raise "a" (lambda ()
+                        (error "a"))))
 
 (test-failure "(assert-raise) fails"
     "raise expected:<a> but was:<b>"
-    (assert-raise 'a (lambda () (raise 'b))))
+    (assert-raise 'a (lambda ()
+                        (raise 'b))))
 
 (test-failure "(assert-raise) fails when no raise"
-    "raise expected:<a> but was:<>"
-    (assert-raise 'a (lambda () (+ 1 1))))
+    "raise expected:<a> but was:<no raise in body>"
+    (assert-raise 'a (lambda ()
+                        (+ 1 1))))
+
+(test-case "(assert-raise) on unbound global variable"
+    (assert-raise 'unbound-global-variable (lambda ()
+                        (unbound-global-variable))))
+
+(test-case "(assert-raise) on type error"
+    (assert-raise "expected number" (lambda ()
+                                        (+ 1 "1"))))
+
+(test-case "(assert-all) allows several assertions"
+    (assert-all
+        (assert-true #t)
+        (assert-true #t)))
+
+(test-failure "(assert-all) evals all assertions"
+    "expected:<true> but was:<false>"
+    (lambda ()
+        (test-case "- inside assert-all"
+            (assert-all
+                (assert-true #t)
+                (assert-true #f)))))
 
 (test-case "(test-case) allows several assertions"
-    (assert-false #f)
-    (assert-false #f))
+    (assert-true #t)
+    (assert-true #t))
+
+(test-failure "(test-case) evals first assertion"
+    "expected:<true> but was:<false>"
+    (lambda ()
+        (test-case "- inside evals first"
+            (assert-true #f)
+            (assert-true #t))))
+
+(test-failure "(test-case) evals second assertion"
+    "expected:<true> but was:<false>"
+    (lambda ()
+        (test-case "- inside evals second"
+            (assert-true #t)
+            (assert-true #f)
+            (assert-true #t))))
+
+(test-failure "(test-case) evals third assertion"
+    "expected:<true> but was:<false>"
+    (lambda ()
+        (test-case "- inside evals third"
+            (assert-true #t)
+            (assert-true #t)
+            (assert-true #f))))
 
 (ignored-test-case "(ignored-test-case) is ignored, else it would fail"
     (assert-true #f))
+
+(ignored-test-case "(ignored-test-case) is not evaluated, else it would crash"
+    (assert-true (unbound-global-variable)))
+
+(test-case "(useless-test-case)"
+    (lambda () #f))
+
+(test-failure "bug (assert-string-list=) fails on extra empty list"
+    "124. item expected:<no more elements> but was:<more elements>"
+    (assert-string-list= '(("4840" ("PRINT" ("\" \"") ";")))
+                         '(("4840" ("PRINT" ("\" \"") ";" ())))))
+
+(test-failure "bug (assert-string-list=) fails on different element after sublist"
+    "3. item expected:<3> but was:<x>"
+    (assert-string-list= '("1" ("2") "3" )
+                         '("1" ("2") "x" )))
