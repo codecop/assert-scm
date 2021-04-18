f0441f5d7344d12bd02b8e8c7f9f1b26bd4baca3
Peter Kofler <peter.kofler@code-cop.org>
2021-04-18 20:14:44 +0200

    Rename Chicken test.

--- test_with_chicken.bat
+++ test_with_chicken.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\ChickenScheme\bin\csi.exe" -r5rs-syntax -q -b assert-test-chicken.scm
+"C:\Program Files (x86)\ChickenScheme\bin\csi.exe" -r5rs-syntax -q -b assert-chicken-test.scm
